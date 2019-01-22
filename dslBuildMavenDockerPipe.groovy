a29ac8961add8e328ac379a71e3fcc4e690ea9c0
Peter <pchen2145@gmail.com>
2019-01-22 14:27:39 +0000

    Loop to create pipelines added

---
+++ dslBuildMavenDockerPipe.groovy
@@ -0,0 +1,45 @@
+// dslBuildMavenDocker.groovy
+
+def gitUrl = "https://github.com/pchen2145/springbootmaven"
+
+for (i = 1; i < 5; i++) {
+
+job("Pipeline_${i}") {
+    description "Pipeline_${i}" {
+        stringParam('dockertag', 'latest', '')
+    }
+
+    scm {
+        git {
+            remote {
+                url gitUrl
+                branch "*/master"
+            }
+
+            extensions {
+                wipeOutWorkspace()
+                localBranch "master"
+            }
+        }
+    }
+
+    steps {
+        maven {
+            goals('package')
+            mavenInstallation('Maven-3.6.0')
+            rootPOM('pom.xml')
+        }
+
+        dockerBuildAndPublish {
+            repositoryName('pchen2145/springbootimage')
+            tag('$dockertag')
+            dockerHostURI('tcp://localhost:2375')
+            registryCredentials('dockerhub')
+            forcePull(true)
+            createFingerprints(true)
+            forceTag(false)
+        }
+    }
+
+}
+}
