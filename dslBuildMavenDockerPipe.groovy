e0eea453efa533551e5c50e886d3e4dc163ca4d4
Peter <pchen2145@gmail.com>
2019-01-22 16:48:23 +0000

    Each loop syntax

--- dslBuildMavenDockerPipe.groovy
+++ dslBuildMavenDockerPipe.groovy
@@ -1,45 +1,48 @@
 // dslBuildMavenDockerPipe.groovy
 
 def gitUrl = "https://github.com/pchen2145/springbootmaven"
+def pipelineList = ["Pipeline 1", "Pipeline2", "Pipeline3"]
 
-for(i in 0..5) {
+println pipelineList
 
-job("Pipeline${i+1}") {
-    description "Pipeline" {
-        stringParam('dockertag', 'latest', '')
-    }
+pipelineList.each {
 
-    scm {
-        git {
-            remote {
-                url gitUrl
-                branch "*/master"
+    job("Pipeline1") {
+        description "Pipeline" {
+            stringParam('dockertag', 'latest', '')
+        }
+
+        scm {
+            git {
+                remote {
+                    url gitUrl
+                    branch "*/master"
+                }
+
+                extensions {
+                    wipeOutWorkspace()
+                    localBranch "master"
+                }
+            }
+        }
+
+        steps {
+            maven {
+                goals('package')
+                mavenInstallation('Maven-3.6.0')
+                rootPOM('pom.xml')
             }
 
-            extensions {
-                wipeOutWorkspace()
-                localBranch "master"
+            dockerBuildAndPublish {
+                repositoryName('pchen2145/springbootimage')
+                tag('$dockertag')
+                dockerHostURI('tcp://localhost:2375')
+                registryCredentials('dockerhub')
+                forcePull(true)
+                createFingerprints(true)
+                forceTag(false)
             }
         }
+
     }
-
-    steps {
-        maven {
-            goals('package')
-            mavenInstallation('Maven-3.6.0')
-            rootPOM('pom.xml')
-        }
-
-        dockerBuildAndPublish {
-            repositoryName('pchen2145/springbootimage')
-            tag('$dockertag')
-            dockerHostURI('tcp://localhost:2375')
-            registryCredentials('dockerhub')
-            forcePull(true)
-            createFingerprints(true)
-            forceTag(false)
-        }
-    }
-
-}
 }
